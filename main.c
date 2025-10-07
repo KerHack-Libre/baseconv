d849782a6b044f3c731d53ffd1c1c8bdde660bb2
Jukoo <jUmarB@protonmail.com>
2025-10-07 12:16:36 +0000

    refresh  build  and core source update

--- main.c
+++ main.c
@@ -27,39 +27,40 @@ int main(int ac , char **av)
   (void) setvbuf(stdout , (void *)0 , _IONBF ,0) ;  
 
   //!NOTICE : when no argument was provided  the repl is automaticaly launched
-  if (!(ac ^1))  
+  if (!(ac &~(1)))  
   { 
     bcrepl_shell(__nptr); 
-    return EXIT_SUCCESS ;
-  }
-    
+    goto _bcv_end; 
+
   //!NOTICE : probably -h and -v (help and version)  
-  if (ac  == 2) 
-  {
+  //!NOTICE : long  options are note handled ... 
+  }else {
     
-    char * short_flags = *(av+ac-1) ; 
-    if( 0x2d ==  *(short_flags)& 0xff) 
+    char * short_flags = *(av+(ac-1)) ; 
+    if( !(0x2d ^  *(short_flags)& 0xff))  
     {
-      switch(*(short_flags+1)) 
+      switch((*(short_flags+1)  & 0xff)) 
       {
-        case 'h': printf("%s%s",USAGE, bcv_version) ;break; 
-        case 'v': printf("%s", BCV_STARTUP_MESG);break; 
+        case 'h': printf("%s\012%s\12",USAGE, BCV_VERSION_STR) ;break; 
+        case 'v': printf("%s\012%s\012", BCV_STARTUP_MESG , BCV_VERSTRLONG);break; 
         default:
                  fprintf(stderr, "unknow option \n");
-                 printf("%s%s",USAGE,  BCV_STARTUP_MESG) ;break; 
+                 printf("%s\012%s\012",USAGE,  BCV_STARTUP_MESG) ;break; 
                  break ; 
       }
     }
 
-  }
+    goto _bcv_end ; 
+  } 
+
   if(ac == 3)
   {
     char *short_flags =  *(av+(ac-2)) ; 
-    int  value = strtol(*(av+(ac-1)) ,  __nptr , 0xa) ; 
+    int  value = strtol(*(av+(ac-1)) ,  __nptr , 012) ; 
    
-    if( 0x2d ==  *(short_flags)& 0xff) 
+    if( !(0x2d ^ *(short_flags)& 0xff))   
     {
-      switch(*(short_flags+1)) 
+      switch( (*(short_flags+1)  &0xff))   
       {
         case 'x': 
           bcv_out(bc_hex(value)); 
@@ -70,15 +71,17 @@ int main(int ac , char **av)
         case 'b': 
           bcv_out(bc_bin(value)); 
           break; 
-        case 'h': printf("%s%s",USAGE, bcv_version) ;break; 
-        case 'v': printf("%s", bcv_version);break; 
+        case 'h': printf("%s\012%s\012",USAGE, BCV_VERSION_STR) ;break; 
+        case 'v': printf("%s\012%s\012", BCV_STARTUP_MESG , BCV_VERSTRLONG);break; 
         default:
                  fprintf(stderr, "unknow option \n");
-                 printf("%s%s",USAGE, bcv_version) ;break; 
+                 printf("%s\012%s\012",USAGE,BCV_VERSION_STR) ;break; 
                  break ; 
       }
     }
   }
+
+_bcv_end: 
 
   return EXIT_SUCCESS ; 
 }
